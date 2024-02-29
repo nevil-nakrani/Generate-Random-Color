acf78eaa78d3cb3e43ba81fdc5063a79835ce713
Nevil-Nakrani <nakaraninevil2017@gmail.com>
2024-03-01 02:11:31 +0530

    Add initial Files

---
+++ script.js
@@ -0,0 +1,23 @@
+let h3=document.querySelector("h3");
+let btn=document.querySelector("#btn");
+let box=document.querySelector("#box");
+
+function AddColor()
+{
+    let r=Math.floor(Math.random()*255);
+    let g=Math.floor(Math.random()*255);
+    let b=Math.floor(Math.random()*255);
+    let a=Math.floor(Math.random()*10)/10;
+
+    let color=`rgba(${r}, ${g}, ${b}, ${a})`;
+    return color;
+}
+
+btn.addEventListener("click",()=>
+{
+    let ranColor=AddColor();
+    h3.innerText=ranColor;
+    box.style.backgroundColor=ranColor;
+
+    console.log("Color Updated Successfully!");
+})
